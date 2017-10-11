752c1364c813d0e4468eb1f6bf2347b0473506cb
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2017-10-11 17:08:38 +0100

    Added endpoint to edit a location

--- flask/app/admin/views.py
+++ flask/app/admin/views.py
@@ -5,7 +5,7 @@ from .forms import LocationPageForm
 import flask
 import flask_login
 
-from flask import flash, redirect
+from flask import flash, redirect, request
 
 @app.route('/admin/dashboard', methods=['GET', 'POST'])
 @flask_login.login_required
@@ -33,5 +33,28 @@ def admin_locations_new():
         db.session.add(page)
         db.session.commit()
         return flask.redirect('/admin/locations')
+    return flask.render_template('admin/location-page-edit.html',
+                                form=form)
+
+@app.route('/admin/locations/<name>', methods=['GET', 'POST'])
+@flask_login.login_required
+def admin_locations_edit(name):
+    form = LocationPageForm()
+    page = LocationPage.query.filter_by(name=name).first()
+    if not page:
+        return flask.abort(404)
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+
+        page.name = name
+        page.description = description
+
+        db.session.add(page)
+        db.session.commit()
+        return flask.redirect('/admin/locations')
+    if request.method == 'GET':
+        form.name.data = page.name
+        form.description.data = page.description
     return flask.render_template('admin/location-page-edit.html',
                                 form=form)
